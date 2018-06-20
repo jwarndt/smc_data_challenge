198866dfb1ad4af4987a1f4ea4396ad99fd82b0a
Jacob Arndt <jakewarndt@gmail.com>
2018-06-19 21:56:31 -0400

    added getters and setters. new main method

--- energy_parser.py
+++ energy_parser.py
@@ -92,3 +92,82 @@ class energy_doc_parser():
     def set_people_density(self):
         self.people_density = float(self.tables[16].contents[-8].contents[-4].get_text())
         self.building_volume = float(self.tables[16].contents[-8].contents[9].get_text())
+
+    def set_all(self):
+        self.set_annual_absolute_energy()
+        self.set_building_area()
+        self.set_window_wall_roof_skylight_info()
+        self.set_people_density()
+        
+    def get_all(self):
+        return [self.total_source_energy,
+        self.total_source_energy_per_building_area,
+        self.total_source_energy_per_conditioned_building_area,
+        self.building_area,
+        self.conditioned_building_area,
+        self.unconditioned_building_area,
+        self.total_wall_area,
+        self.total_wall_area_N,
+        self.total_wall_area_E,
+        self.total_wall_area_S,
+        self.total_wall_area_W,
+        self.total_window_area,
+        self.total_window_area_N,
+        self.total_window_area_E,
+        self.total_window_area_S,
+        self.total_window_area_W,
+        self.total_above_ground_win_wall_ratio,
+        self.total_above_ground_win_wall_ratio_N,
+        self.total_above_ground_win_wall_ratio_E,
+        self.total_above_ground_win_wall_ratio_S,
+        self.total_above_ground_win_wall_ratio_W,
+        self.roof_area,
+        self.skylight_area,
+        self.roof_skylight_ratio,
+        self.people.density,
+        self.building_volume]
+
+
+file_dir = "D:/ornl/urban_challenge_dataset/energy"
+def process_building_energy_info(file_dir, output_csv):
+    filelist = []
+    for dirpath, dnames, fnames in os.walk(file_dir):
+        for f in fnames:
+            if f.endswith(".htm"):
+                filelist.append(os.path.join(dirpath, f))
+    with open(output_csv, 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile, delimiter=' ',
+                                quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        writer.writerow(['ID',
+                         'node',
+                         'tsa',
+                         'tsapba',
+                         'tsapcba',
+                         'building_area',
+                         'conditioned_building_area',
+                         'unconditioned_building_area',
+                         'total_wall_area',
+                         'total_wall_area_N',
+                         'total_wall_area_E',
+                         'total_wall_area_S',
+                         'total_wall_area_W',
+                         'total_window_area',
+                         'total_window_area_N',
+                         'total_window_area_E',
+                         'total_window_area_S',
+                         'total_window_area_W',
+                         'total_above_ground_win_wall_ratio',
+                         'total_above_ground_win_wall_ratio_N',
+                         'total_above_ground_win_wall_ratio_E',
+                         'total_above_ground_win_wall_ratio_S',
+                         'total_above_ground_win_wall_ratio_W',
+                         'roof_area',
+                         'skylight_area',
+                         'roof_skylight_ratio',
+                         'people.density',
+                         'building_volume'])
+        for energy_doc in filelist:
+            en = energy_doc_parser(energy_doc)
+            en.set_all()
+            results = en.get_all()
+            writer.writerow(results)
